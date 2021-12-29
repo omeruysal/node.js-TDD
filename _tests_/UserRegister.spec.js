5961ae8ab73d6fa914d92a3158dc3dacef5de78e
Omer <teouysal@outlook.com>
2021-12-29 23:05:25 +0300

    added new test cases and validations

--- _tests_/UserRegister.spec.js
+++ _tests_/UserRegister.spec.js
@@ -12,31 +12,35 @@ beforeEach(() => {
   return User.destroy({ truncate: true }); //Before each test method we clear our db , truncate?
 });
 
-describe('User Registration', () => {
-  const postValidUser = () => {
-    return request(app).post('/api/1.0/users').send({
-      username: 'user1',
-      email: 'user1@mail.com',
-      password: 'Password1',
-    });
-  };
+const validUser = {
+  username: 'user1',
+  email: 'user1@mail.com',
+  password: 'Password1',
+};
 
+const postUser = (user = validUser) => {
+  return request(app).post('/api/1.0/users').send(user);
+};
+
+describe('User Registration', () => {
   it('returns 200 OK when signup request is valid', async () => {
-    const response = await postValidUser();
+    const response = await postUser();
     expect(response.status).toBe(200); //this expect comes with jest not supertest
   });
 
   it('returns success message when signup request is valid', async () => {
-    const response = await postValidUser();
+    const response = await postUser();
     expect(response.body.message).toBe('User created');
   });
+
   it('saves the user to database', async () => {
-    await postValidUser();
+    await postUser();
     const userList = await User.findAll();
     expect(userList.length).toBe(1);
   });
+
   it('saves the username and email to database', async () => {
-    await postValidUser();
+    await postUser();
     const userList = await User.findAll();
     const savedUser = userList[0];
     expect(savedUser.username).toBe('user1');
@@ -44,9 +48,102 @@ describe('User Registration', () => {
   });
 
   it('hashes the password in database', async () => {
-    await postValidUser();
+    await postUser();
     const userList = await User.findAll();
     const savedUser = userList[0];
     expect(savedUser.password).not.toBe('Password1');
   });
+
+  it('returns 400 when username is null', async () => {
+    const response = await postUser({
+      username: null,
+      email: 'user1@mail.com',
+      password: 'Password1',
+    });
+    expect(response.status).toBe(400);
+  });
+
+  it('returns validationErrors field in response body when validation error occurs', async () => {
+    const response = await postUser({
+      username: null,
+      email: 'user1@mail.com',
+      password: 'Password1',
+    });
+    const body = response.body;
+    expect(body.validationErrors).not.toBeUndefined();
+  });
+
+  it('returns errors for both when username and email are null', async () => {
+    const response = await postUser({
+      username: null,
+      email: null,
+      password: 'Password1',
+    });
+    const body = response.body;
+    expect(Object.keys(body.validationErrors)).toEqual(['username', 'email']);
+  });
+
+  it.each([
+    //with it.each() we can run dynamic tests at once
+    ['username', null, 'Username can not be null'],
+    ['username', 'usr', 'Must have min 4 and max 32 characters'],
+    ['username', 'a'.repeat(33), 'Must have min 4 and max 32 characters'],
+    ['email', null, 'Email can not be null'],
+    ['email', 'mail.com', 'Email is not valid'],
+    ['email', 'user.mail.com', 'Email is not valid'],
+    ['email', 'user@mail', 'Email is not valid'],
+    ['password', null, 'Password can not be null'],
+    ['password', 'P4ssw', 'Password must be at least 6 characters'],
+  ])('when %s is  %s, %s is received', async (field, value, expectedMessage) => {
+    const user = {
+      username: 'user1',
+      email: 'user1@email.com',
+      password: 'Password1',
+    };
+    user[field] = value;
+    const response = await postUser(user);
+    const body = response.body;
+    expect(body.validationErrors[field]).toBe(expectedMessage);
+  });
+
+  //  --------------- Created it.each() for the below test cases ----------------
+  //   it('returns size validation error when username is less than 4 characters', async () => {
+  //     const user = {
+  //       username: 'usr',
+  //       email: 'user1@email.com',
+  //       password: 'Password1',
+  //     };
+  //     const response = await postUser(user);
+  //     const body = response.body;
+  //     expect(body.validationErrors.username).toBe('Must have min 4 and max 32 characters');
+  //   });
+  //   it('returns Username can not be null when username is null', async () => {
+  //     const response = await postUser({
+  //       username: null,
+  //       email: 'user1@mail.com',
+  //       password: 'Password1',
+  //     });
+  //     const body = response.body;
+  //     expect(body.validationErrors.username).toBe('Username can not be null');
+  //   });
+
+  //   it('returns Email can not be null when email is null', async () => {
+  //     const response = await postUser({
+  //       username: 'user1',
+  //       email: null,
+  //       password: 'Password1',
+  //     });
+  //     const body = response.body;
+  //     expect(body.validationErrors.email).toBe('Email can not be null');
+  //   });
+
+  //   it('returns Password can not be null when password is null', async () => {
+  //     const response = await postUser({
+  //       username: 'user1',
+  //       email: 'user@mail.com',
+  //       password: null,
+  //     });
+  //     const body = response.body;
+  //     expect(body.validationErrors.password).toBe('Password can not be null');
+  //   });
 });
--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -1,12 +1,57 @@
 const express = require('express');
 const router = express.Router();
 const UserService = require('../user/UserService');
+const { check, validationResult } = require('express-validator');
 
-router.post('/api/1.0/users', async (req, res) => {
-  try {
+router.post(
+  '/api/1.0/users',
+  check('username')
+    .notEmpty()
+    .withMessage('Username can not be null')
+    .bail() // if username is null then do not go on
+    .isLength({ min: 4, max: 32 })
+    .withMessage('Must have min 4 and max 32 characters'),
+  check('email').notEmpty().withMessage('Email can not be null').bail().isEmail().withMessage('Email is not valid'),
+  check('password')
+    .notEmpty()
+    .withMessage('Password can not be null')
+    .bail()
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters'),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const validationErrors = {};
+      errors.array().forEach((error) => (validationErrors[error.param] = error.msg));
+      return res.status(400).send({ validationErrors: validationErrors });
+    }
     await UserService.save(req.body);
     return res.send({ message: 'User created' });
-  } catch (error) {}
-});
+  }
+);
 
 module.exports = router;
+
+// const validateUsername = (req, res, next) => {
+//     const user = req.body;
+//     if (user.username === null) {
+//       return res.status(400).send({
+//         validationErrors: {
+//           username: 'Username can not be null',
+//         },
+//       });
+//     }
+//     next();
+//   };
+
+//   const validateEmail = (req, res, next) => {
+//     const user = req.body;
+//     if (user.email === null) {
+//       return res.status(400).send({
+//         validationErrors: {
+//           email: 'Email can not be null',
+//         },
+//       });
+//     }
+//     next();
+//   };
