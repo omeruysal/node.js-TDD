05d26ca6c4b8359698e5b766b34a940ebc5c92bd
Omer <teouysal@outlook.com>
2022-04-14 22:26:23 +0300

    updates

--- src/user/UserService.js
+++ src/user/UserService.js
@@ -2,6 +2,10 @@ const User = require('../user/User');
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
 const EmailService = require('../email/EmailService');
+const sequelize = require('../config/database');
+const EmailException = require('../email/EmailException');
+const InvalidTokenException = require('./InvalidTokenException');
+
 const generateToken = (length) => {
   return crypto.randomBytes(length).toString('hex').substring(0, length);
 };
@@ -10,12 +14,30 @@ const save = async (body) => {
   const { username, email, password } = body;
   const hash = await bcrypt.hash(password, 10);
   const user = { username, email, password: hash, activationToken: generateToken(16) };
-  await User.create(user);
+  const transaction = await sequelize.transaction();
+  await User.create(user, { transaction });
 
-  await EmailService.sendAccountActivation(email, user.activationToken);
+  try {
+    await EmailService.sendAccountActivation(email, user.activationToken);
+    await transaction.commit();
+  } catch (error) {
+    console.log(error);
+    await transaction.rollback();
+    throw new EmailException();
+  }
 };
 
 const findyByEmail = async (email) => {
   return await User.findOne({ where: { email } });
 };
-module.exports = { save, findyByEmail };
+
+const activate = async (token) => {
+  const user = await User.findOne({ where: { activationToken: token } });
+  if (!user) {
+    throw new InvalidTokenException();
+  }
+  user.inactive = false;
+  user.activationToken = null;
+  await user.save();
+};
+module.exports = { save, findyByEmail, activate };
