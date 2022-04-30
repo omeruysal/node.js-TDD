6b693c366f0131e4e4e4f8450567305dca94b80b
Omer <teouysal@outlook.com>
2022-04-30 17:50:01 +0300

    added error handler

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const UserRouter = require('../src/user/UserRouter');
+const ErrorHandler = require('./error/ErrorHandler');
 
 const app = express();
 
 app.use(express.json());
 app.use(UserRouter);
+app.use(ErrorHandler);
 
 module.exports = app;
